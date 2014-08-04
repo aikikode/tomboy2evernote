eaa781f3ab085c5dd3506798e2b0f6e5b89089e4
aikikode <aikikode@gmail.com>
2014-08-04 19:47:16 +0400

    Add tomboy notes converter

--- tomboy2evernote/tomboy2evernote.py
+++ tomboy2evernote/tomboy2evernote.py
@@ -1,15 +1,19 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import glob
 import os
+import re
 from evernote.api.client import EvernoteClient
 from evernote.edam.notestore.ttypes import NoteFilter, NotesMetadataResultSpec
-from evernote.edam.type.ttypes import Note
+from evernote.edam.type.ttypes import Note, Notebook
+import lxml.etree as ET
 
 __author__ = 'aikikode'
 
 EVERNOTE_HEADER = ("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
                    "<!DOCTYPE en-note SYSTEM \"http://xml.evernote.com/pub/enml2.dtd\">\n")
+EV_DATE_FORMAT = '%Y%m%dT%H%M%SZ'
 TOMBOY_DIR = os.path.join(os.environ['HOME'], ".local", "share", "tomboy")
-
 
 class Evernote(EvernoteClient):
     def __init__(self, token):
@@ -36,20 +40,99 @@ class Evernote(EvernoteClient):
                 break
         else:
             note = Note()
-            note.title = note_title
-            note.content = note_contents
+            note.title, note.content = note_title, note_contents
             for notebook in self.note_store.listNotebooks():
                 if notebook.name == notebook_name:
                     notebook_guid = notebook.guid
                     note.notebookGuid = notebook_guid
                     break
             else:
-                # TODO: create notebook
-                notebook_guid = None
+                if notebook_name:
+                    # Notebook not found, create new one
+                    notebook = Notebook()
+                    notebook.name = notebook_name
+                    notebook = self.note_store.createNotebook(notebook)
+                    note.notebookGuid = notebook.guid
                 self.note_store.createNote(note)
+
+    def cat_note(self, note_title):
+        notes_data_list = self.note_store.findNotesMetadata(NoteFilter(words="intitle:\"{}\"".format(note_title)), 0,
+                                                            100, NotesMetadataResultSpec())
+        notes = (self.note_store.getNote(note_data.guid, True, False, False, False)
+                 for note_data in notes_data_list.notes)
+        for note in notes:
+            if note.title == note_title:
+                print note.content
+                break
+
+
+def convert_tomboy_to_evernote(note_path):
+    def el(name, parent=''):
+        return "{{http://beatniksoftware.com/tomboy{}}}{}".format(parent, name)
+
+    def innertext(tag):
+        """Convert Tomboy XML to Markdown"""
+        text = tag.text or ''
+        tail_text = ''
+        tags_convertion = {el('bold'): 'strong',
+                           el('underline'): ['span style="text-decoration: underline;"', 'span'],
+                           el('monospace'): ['span style="font-family: \'courier new\', courier, monospace;"', 'span'],
+                           el('url', '/link'): ['a href="{}"', 'a'],
+                           el('list'): 'ul',
+                           el('list-item'): 'li',
+                           }
+        try:
+            ev_tag = tags_convertion[tag.tag]
+            if isinstance(ev_tag, list):
+                text = '<{}>{}</{}>'.format(ev_tag[0].format(text), text, ev_tag[1])
+                tail_text = '{}'.format(tag.tail or '')
+            else:
+                text = '<{}>{}'.format(ev_tag, text)
+                if ev_tag == 'ul':
+                    tail_text = '</{}>{}'.format(ev_tag, tag.tail or '')
+                else:
+                    tail_text = '{}</{}>'.format(tag.tail or '', ev_tag)
+        except KeyError:
+            pass
+        return "{}{}{}".format(text, ''.join(innertext(e) for e in tag), tail_text)
+    TOMBOY_CAT_PREFIX = 'system:notebook:'
+    root = ET.parse(note_path).getroot()
+
+    tags = []
+    notebook = None
+
+    tagsEl = root.find(el('tags'))
+    if tagsEl is not None:
+        for tagEl in tagsEl:
+            tag = tagEl.text
+            if tag.startswith(TOMBOY_CAT_PREFIX):
+                notebook = tag.replace(TOMBOY_CAT_PREFIX, '')
+            else:
+                tags.append(tag)
+    if 'system:template' in tags:
+        return None
+
+    note = {}
+
+    title = root.find(el('title')).text
+
+    # Parse contents
+    contentTag = root.find(el('text')).find(el('note-content'))
+    content = innertext(contentTag)
+    content = content.replace(title, '', 1).lstrip()
+    content = ''.join(['{}<br clear="none"/>'.format(line.strip()) if re.match('<.*>', line.strip()) else '<div>{}<br clear="none"/></div>'.format(line.strip()) for line in content.split('\n')])
+
+    note['title'] = title
+    note['content'] = content
+    note['tags'] = tags
+    note['notebook'] = notebook
+
+    return note
 
 
 if __name__ == "__main__":
     dev_token = ""
     evernote = Evernote(token=dev_token)
-    evernote.create_or_update_note("Test note 123", "12345<h1>title</h1>new line")
+    for tomboy_note in glob.glob(os.path.join(TOMBOY_DIR, "*.note")):
+        note = convert_tomboy_to_evernote(tomboy_note)
+        evernote.create_or_update_note(note['title'], note['content'], note['notebook'])
